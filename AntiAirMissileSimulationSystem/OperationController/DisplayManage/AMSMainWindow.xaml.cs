4eeaf7a996c4046ebd0cca134a412b49363b778e
DESKTOP-3EAE6D3\User <choi.jupiter@gmail.com>
2024-04-22 11:08:23 +0900

    초기 프로젝트

---
+++ AntiAirMissileSimulationSystem/OperationController/DisplayManage/AMSMainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using OperationController.Data;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OperationController.DisplayManage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AMSMainWindow : Window
+    {
+        public AMSMainWindow()
+        {
+            InitializeComponent();
+            EventLog.Text += "\n";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            EventLog.Text += AMSConfiguration.GetInstance().Port() + "\n";
+            EventLog.Text += AMSConfiguration.GetInstance().UpdateDuration() + "\n";
+        }
+    }
+}
