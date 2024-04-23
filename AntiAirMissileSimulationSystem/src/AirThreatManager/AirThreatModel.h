7cf0f1c637a32d2fe8887d621d83003a5aec03d0
DESKTOP-K5CCIJG\User <zmffhqj35@naver.com>
2024-04-23 14:15:19 +0900

    class position 추가 및 코드 수정

--- AntiAirMissileSimulationSystem/src/AirThreatManager/AirThreatModel.h
+++ AntiAirMissileSimulationSystem/src/AirThreatManager/AirThreatModel.h
@@ -2,35 +2,42 @@
 #include <chrono>
 #include <functional>
 
-// class 선언부
+class position
+{
+public:
+    position(double x, double y) : x(x), y(y) {}
+
+    double x;
+    double y;
+};
+
 class AirthreatModel
 {
 public:
-	AirthreatModel();
+    AirthreatModel();
 
-	void GetCurrentAirThreat(double stx, double sty, double speed, double edx, double edy);
-	void GetCurrentPosition();
-	double GetCurrenAngle();
-	void GetScenarioInfo(int info);
-	int PubScenarioInfo();
-	void start();
-	void stop();
-
+    void GetCurrentAirThreat(double stx, double sty, double speed, double edx, double edy);
+    void GetCurrentPosition();
+    double GetCurrenAngle();
+    void GetScenarioInfo(int info);
+    int PubScenarioInfo();
+    void start();
 
 private:
-	// startAirThreat Info
-	double initX, initY, initSpeed;
-	double targetX, targetY;
-	// currentAirThreat Info
-	double currentX, currentY;
 
-	bool isThreadRunning = false;
-	int scenarioState = 1;
-	//IDLE=0, DETECTEING=1,CHASING=2, SUCCESS=3, FAIL=4
-	//※ 동작 정보 : 공중위협 발사, 대공유도탄 발사, 시나리오 중지, 공중위협 탐지 및 추적, 요격 성공, 요격 실패
+    // startAirThreat Info
+    double initSpeed;
+    position initPos = position(0, 0);
+    position targetPos = position(0, 0);
+    // currentAirThreat Info
+    position currentPos = position(0, 0);
+
+    bool isThreadRunning = false;
+    // IDLE=0, DETECTEING=1, CHASING=2, SUCCESS=3, FAIL=4
+    // ※ 동작 정보 : 공중위협 발사, 대공유도탄 발사, 시나리오 중지, 공중위협 탐지 및 추적, 요격 성공, 요격 실패
+    int scenarioState = 1;
 
 private:
-	void threatSimulationThread();
-	void updateThreatPosition();
+    void threatSimulationThread();
+    void updateThreatPosition();
 };
-
--- AntiAirMissileSimulationSystem/src/AirThreatManager/main.cpp
+++ AntiAirMissileSimulationSystem/src/AirThreatManager/main.cpp
@@ -1,121 +1,103 @@
-#include "AirthreatModel.h"
-#include <math.h>
+#include "AirThreatModel.h"
+#include <cmath>
 #include <iostream>
 
 // 초기화
 AirthreatModel::AirthreatModel()
 {
-	initX = 0;
-	initY = 0;
-	initSpeed = 0;
-	targetX = 0;
-	targetY = 0;
+    initPos = position(0, 0);
+    initSpeed = 0;
+    targetPos = position(0, 0);
 }
 
-
-// class 구현부
 // 설정한 공중위협 정보 가져오기 (출발지, 속도, 목적지)
 void AirthreatModel::GetCurrentAirThreat(double stx, double sty, double speed, double edx, double edy)
 {
-	initX = stx;
-	initY = sty;
-	initSpeed = speed / 3600; //[km/h]
-	targetX = edx;
-	targetY = edy;
+    initPos = position(stx, sty);
+    initSpeed = speed / 3600; //[km/h]
+    targetPos = position(edx, edy);
 }
-
 
 // 시나리오 상태 업데이트
 // 요격되면 공중위협 멈추도록
 void AirthreatModel::GetScenarioInfo(int info)
 {
-	scenarioState = info;
+    scenarioState = info;
 }
 
 // 시나리오 상태 내보내기
 // 목적지 도착
 int AirthreatModel::PubScenarioInfo()
 {
-	std::cout << "scenarioState : " << scenarioState << std::endl;
-	return scenarioState;
-
+    std::cout << "scenarioState : " << scenarioState << std::endl;
+    return scenarioState;
 }
-
 
 void AirthreatModel::start()
 {
-	// 초기 위치 설정
-	currentX = initX;
-	currentY = initY;
+    // 초기 위치 설정
+    currentPos = initPos;
 
-	// detecting이거나 chasing이면 출발
-	// 시나리오 이벤트를 실시간으로 받아오는 것 추가해야함
-	// GetCurrentPosition(info);
-	if (scenarioState == 1 | scenarioState == 2)
-	{
-		isThreadRunning = true;
-	}
-	while (isThreadRunning) {
+    // detecting이거나 chasing이면 출발
+    // 시나리오 이벤트를 실시간으로 받아오는 것 추가해야함
+    if (scenarioState == 1 || scenarioState == 2)
+    {
+        isThreadRunning = true;
+    }
+    while (isThreadRunning)
+    {
 
-		if ((initX < targetX && currentX >= targetX) || (initX > targetX && currentX <= targetX)) // 목적지 도착. fail
-		{
-			isThreadRunning = false;
-			scenarioState = 4;
-			PubScenarioInfo();
-			break;
-		}
-		if (scenarioState == 3) // 요격되면 
-		{
-			isThreadRunning = false;
-			break;
-		}
-		// 아니면 계속 좌표 업데이트
-		GetCurrentPosition();
-	}
+        if ((initPos.x < targetPos.x && currentPos.x >= targetPos.x) || (initPos.x > targetPos.x && currentPos.x <= targetPos.x)) // 목적지 도착. fail
+        {
+            isThreadRunning = false;
+            scenarioState = 4;
+            PubScenarioInfo();
+            break;
+        }
+        if (scenarioState == 3) // 요격되면 
+        {
+            isThreadRunning = false;
+            break;
+        }
+        // 아니면 계속 좌표 업데이트
+        GetCurrentPosition();
+    }
 }
-
 
 // 방향 계산
 double AirthreatModel::GetCurrenAngle()
 {
-	double deltaX, deltaY;
-	double theta;
+    double deltaX = targetPos.x - initPos.x;
+    double deltaY = targetPos.y - initPos.y;
 
-	deltaX = targetX - initX;
-	deltaY = targetY - initY;
+    if (deltaX == 0.0 || deltaY == 0.0)
+        return 0;
 
-	if (deltaX == 0.0 || deltaY == 0.0)
-		return 0;
-
-	theta = abs(atan(deltaY / deltaX));
-	return theta;
+    double theta = std::abs(std::atan(deltaY / deltaX));
+    return theta;
 }
 
 // 좌표 계산
 void AirthreatModel::GetCurrentPosition()
 {
-	double theta = GetCurrenAngle();
+    double theta = GetCurrenAngle();
 
-	if (initX < targetX)
-		currentX = currentX + initSpeed * cos(theta);
-	else
-		currentX = currentX - initSpeed * cos(theta);
+    if (initPos.x < targetPos.x)
+        currentPos.x = currentPos.x + initSpeed * std::cos(theta);
+    else
+        currentPos.x = currentPos.x - initSpeed * std::cos(theta);
 
-	if (initY < targetY)
-		currentY = currentY + initSpeed * sin(theta);
-	else
-		currentY = currentY - initSpeed * sin(theta);
+    if (initPos.y < targetPos.y)
+        currentPos.y = currentPos.y + initSpeed * std::sin(theta);
+    else
+        currentPos.y = currentPos.y - initSpeed * std::sin(theta);
 
-	std::cout << "Threat position : (" << currentX << ", " << currentY << ")" << std::endl;
+    std::cout << "Threat position : (" << currentPos.x << ", " << currentPos.y << ")" << std::endl;
 }
 
-
-// ----------------------------------------------------------------------------
 int main() {
-	AirthreatModel atm;
-	atm.GetCurrentAirThreat(30, 40, 100, 0, 10);
-	atm.GetScenarioInfo(1);
-	atm.start();
-	///int pub_scenario = atm->scenarioState;
+    AirthreatModel atm;
+    atm.GetCurrentAirThreat(30, 40, 100, 0, 10);
+    atm.GetScenarioInfo(1);
+    atm.start();
 }
-
